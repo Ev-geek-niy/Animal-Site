33fbe722912d5a4d3967f87c760fabbc7f790baa
Ev-geek-niy <ev_geek_niy@vk.com>
2026-02-15 17:36:28 +0500

    Начала разработки сайта, блок слайдера
    - Реализована начало верстки сайта: шапка, контентная часть
    - Добавлена возможность прикреплять свои изображения в слайдер
    - Выпадающее меню, модальное окно и слайдер перенесены в отдельную папку template, чтобы было удобнее смотреть на пример и копировать его
    - Изменены переменные для цветов на более теплые, добавлены новые стили для исправления проблем с версткой

--- js/slider.js
+++ js/slider.js
@@ -1,15 +1,29 @@
 document.querySelector(".slider-btn.prev").addEventListener("click", prevSlide);
 document.querySelector(".slider-btn.next").addEventListener("click", nextSlide);
+// document.addEventListener("DOMContentLoaded", autoplay)
 
-const slides = [...document.querySelectorAll(".slide")];
-const sliderCounter = document.querySelector(".slider-counter");
+const sliderFileInput = document.querySelector('.slider-add-image-btn')
+sliderFileInput.onchange = (e) => addImage(e.target.files[0]);
 
-slides.forEach(slide => {
-  sliderCounter.insertAdjacentHTML("beforeend", `<div class="dot"></div>`)
-})
+const sliderCarousel = document.querySelector('.slider-carousel');
+let slides = null;
+let sliderCounter = null;
+let dots = null;
 
-const dots = document.querySelectorAll(".dot");
-dots[0].classList.add("active");
+document.addEventListener('DOMContentLoaded', () => setupSlider())
+
+function setupSlider() {
+  slides = [...document.querySelectorAll(".slide")];
+  sliderCounter = document.querySelector(".slider-counter");
+
+  sliderCounter.innerHTML = "";
+  slides.forEach(slide => {
+    createSliderDot(sliderCounter)
+  })
+
+  dots = document.querySelectorAll(".dot");
+  dots[0].classList.add("active");
+}
 
 function prevSlide() {
   const currentSlideIndex = slides.findIndex(slide => slide.classList.contains("active"))
@@ -41,4 +55,23 @@ function nextSlide() {
 
   currentSlide.classList.remove("active");
   dots[currentSlideIndex].classList.remove("active");
+}
+
+function createSliderDot(counterDiv) {
+  counterDiv.insertAdjacentHTML("beforeend", `<div class="dot"></div>`);
+}
+
+function autoplay() {
+  setInterval(() => nextSlide(), 3000);
+}
+
+function addImage(file) {
+  const imageUrl = URL.createObjectURL(file);
+
+  sliderCarousel.insertAdjacentHTML("beforeend", `
+    <div class="slide">
+        <img src=${imageUrl}
+             alt="Ваше изображение"/>
+    </div>`);
+  setupSlider()
 }
