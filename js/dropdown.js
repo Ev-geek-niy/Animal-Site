d8587bb9e6a21c1a943c1c3bdc8d1603dafdf7ec
Ev-geek-niy <ev_geek_niy@vk.com>
2026-02-17 21:41:03 +0500

    Добавил поддержку выпадающих списков в шапку сайта
    - Теперь текст из подменю запоминается при нажатии

--- js/dropdown.js
+++ js/dropdown.js
@@ -1,6 +1,5 @@
-document.querySelectorAll(".dropdown-header").forEach(function (button) {
-  button.addEventListener("click", function () {
-    const dropdown = this.closest(".dropdown");
-    dropdown.classList.toggle("open");
-  });
-});
+// document.querySelectorAll(".dropdown").forEach(function (dropdown) {
+//   dropdown.addEventListener("click", function () {
+//     dropdown.classList.toggle("open");
+//   });
+// });
